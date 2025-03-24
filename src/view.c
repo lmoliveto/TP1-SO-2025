606ef6976480d6f240b200d8fa7250a8312f4189
Tomás Pietravallo <hello@tomaspietravallo.com>
2025-03-24 19:24:37 -0300

    feat(view): semaphone sync with master

--- src/view.c
+++ src/view.c
@@ -12,6 +12,7 @@
 #define ANSI_HIGH_INTENSITY_GREEN "\x1b[92m"
 #define ANSI_HIGH_INTENSITY_BLUE "\x1b[94m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
+#define ANSI_CLEAR_SCREEN "\033[H\033[J"
 
 static const char * colors[] = {
     ANSI_COLOR_RED,
@@ -25,20 +26,36 @@ static const char * colors[] = {
     ANSI_HIGH_INTENSITY_BLUE
 };
 
-int main (void) {
-    Board * game_board = (Board *) accessSHM("/game_state", sizeof(Board), O_RDONLY, 0, PROT_READ);
-    
+void print_board(Board * game_board) {
+    printf(ANSI_CLEAR_SCREEN);
     for (int y = 0; y < game_board->height; y++) {
         for (int x = 0; x < game_board->width; x++) {
             int cell_value = game_board->cells[x + y * game_board->width];
             if (cell_value > 0) {
-                printf(" %d  ", cell_value);
+                printf("%d   ", cell_value);
             } else {
-                printf(" %s%c%s  ", colors[-cell_value], '#', ANSI_COLOR_RESET);
+                printf("%s%c%s   ", colors[-cell_value], '#', ANSI_COLOR_RESET);
             }
         }
         printf("\n\n");
     }
+}
+
+int main (void) {
+    Board * game_board = (Board *) accessSHM("/game_state", sizeof(Board), O_RDONLY, 0, PROT_READ);
+    Semaphores * game_sync = (Semaphores *) accessSHM("/game_sync", sizeof(Semaphores), O_RDWR, 0, PROT_READ | PROT_WRITE);
+    
+    short finished = 0;
+
+    do {
+        sem_wait(&game_sync->has_changes);
+        sem_wait(&game_sync->game_state);
+        finished = game_board->finished;
+        sem_post(&game_sync->game_state);
+
+        print_board(game_board);
+        sem_post(&game_sync->print_done);
+    } while (!finished);
 
     return 0;
 }
