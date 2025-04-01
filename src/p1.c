8ca320c396a1ddcb3f40de1bcdf88bc980e25c76
Tomás Pietravallo <hello@tomaspietravallo.com>
2025-04-01 19:25:14 -0300

    fix: assert move was registered by master

--- src/p1.c
+++ src/p1.c
@@ -16,38 +16,64 @@ int main (int argc, char* argv[]) {
     Board * game_board = (Board *) accessSHM("/game_state", sizeof(Board) + sizeof(int) * width * height, O_RDONLY, 0, PROT_READ);
     Semaphores * game_sync = (Semaphores *) accessSHM("/game_sync", sizeof(Semaphores), O_RDWR, 0, PROT_READ | PROT_WRITE);
 
+    int prev_count = -1;
+    int player_id = -1;
+    int skip_write = 0;
+    int pid = getpid();
+    
+    for (int i = 0; i < MAX_PLAYERS; i++) {
+        if (game_board->players[i].pid == pid) {
+            player_id = i;
+            break;
+        }
+    }
+
+    if (player_id == -1) {
+        fprintf(stderr, "Error: Player not found\n");
+        return EXIT_FAILURE;
+    }
+
     while (!game_board->finished) {
-        // ENTRY SECTION
-        sem_wait(&game_sync->players_count_mutex); // protects players_reading
+        sem_wait(&game_sync->players_done);
+        sem_post(&game_sync->players_done);
+
+        sem_wait(&game_sync->players_count_mutex);
         game_sync->players_reading++;
         if (game_sync->players_reading == 1) {
             sem_wait(&game_sync->sync_state);
         }
         sem_post(&game_sync->players_count_mutex);
 
-        // CRITICAL SECTION
-        // ... read the state of the game board
-        // ... decide on the best move ...
 
-        char buff[1];
-        // buff[0] = 4;
-        buff[0] = rand() % DIR_NUM;
-        int written = write(STDOUT_FILENO, buff, 1);
-        
-        if (1 != written) {
-            perror("child: write");
-            exit(EXIT_FAILURE);
-        }
-        
-        // EXIT SECTION
+        // todo consultar estado
+
+        int count = game_board->players[player_id].valid_move_count + game_board->players[player_id].invalid_move_count;
+        skip_write = count == prev_count;
+        prev_count = count;
+
         sem_wait(&game_sync->players_count_mutex);
         game_sync->players_reading--;
         if (game_sync->players_reading == 0) {
             sem_post(&game_sync->sync_state);
-            sem_post(&game_sync->players_done);
         }
         sem_post(&game_sync->players_count_mutex);
-        usleep(10000);
+        sem_post(&game_sync->players_done);
+        // usleep(10000);
+
+        // todo decidir siguiente moviemiento
+        
+        if (!skip_write) {
+            // enviar movimiento
+            char buff[1];
+            // buff[0] = 4;
+            buff[0] = rand() % DIR_NUM;
+            int written = write(STDOUT_FILENO, buff, 1);
+            if (1 != written) {
+                perror("child: write");
+                exit(EXIT_FAILURE);
+            }
+        }
+
     }
 
     exit(EXIT_SUCCESS);
