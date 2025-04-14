8323dcb6557a8333774473922001fc9800e59494
Tomás Pietravallo <hello@tomaspietravallo.com>
2025-04-14 11:30:22 -0300

    chore: add file prefixes

--- src/player.c
+++ src/player.c
@@ -1,3 +1,6 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
 // Compile with -DSTRATEGY_* to select which strategy to use, if none is selected then the player chooses a random direction
 
 #include "constants.h"
